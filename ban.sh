9930a67181661ce8d1d5a0d30537cd1f0119320d
kolyanok <kolyan-okunok@ya.ru>
2016-10-16 19:30:35 +0300

    Using raw.githubusercontent.com by default
    Лучше использовать raw.githubusercontent.com — он стабильнее и куда быстрее обновляется после изменения файла на git.

--- ban.sh
+++ ban.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wget -O - http://cdn.rawgit.com/TTLMaster/yota-ban-list/master/list.txt | while read -r i; do
+#wget -O - http://cdn.rawgit.com/TTLMaster/yota-ban-list/master/list.txt | while read -r i; do
+wget -O - https://raw.githubusercontent.com/TTLMaster/yota-ban-list/master/list.txt | while read -r i; do
 	[ "`echo $i | sed -n '/^#.*$/p'`" ] && continue
 	iptables -I FORWARD --destination $i -j REJECT;
 done
