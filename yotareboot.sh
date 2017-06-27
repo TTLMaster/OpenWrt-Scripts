dbea25f99a1a78384a6a9ef30d0bf49ffe078bbc
kolyanok <kolyanok@users.noreply.github.com>
2017-06-27 22:00:34 +0300

    Update 2

--- yotareboot.sh
+++ yotareboot.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 for i in 1 2 3 4 5
 do
-	content=$(wget captive.apple.com -q -O -)
-	if [ `echo $content | grep -c "tethering.svg" ` -gt 0 ]
+	content=$(wget -O /dev/null http://captive.apple.com 2>&1)
+	if [ `echo $content | grep -c "phone-tethering"` -gt 0 ]
 	then
 		usbreset "HUAWEI Mobile"
 	fi
 	sleep 10
 done
-
