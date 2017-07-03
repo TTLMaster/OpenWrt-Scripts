5891115f8a408bd3ef106bb30cafbad482fc948d
Ilya Fedin <fedin-ilja2010@ya.ru>
2017-07-03 23:56:31 +0400

    Определение тетеринга на планшетных тарифах

--- yotareboot.sh
+++ yotareboot.sh
@@ -2,7 +2,7 @@
 for i in 1 2 3 4 5
 do
 	content=$(wget -O /dev/null http://captive.apple.com 2>&1)
-	if [ `echo $content | grep -c "phone-tethering"` -gt 0 ]
+	if [ `echo $content | grep -cE "(phone-tethering|tablet-tethering)"` -gt 0 ]
 	then
 		usbreset "HUAWEI Mobile"
 	fi
