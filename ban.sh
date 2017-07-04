07a2956b9f048e73e1fbd32d92612c86ef1fb633
Ilya Fedin <fedin-ilja2010@ya.ru>
2017-07-04 04:43:37 +0400

    Небольшое упрощение

--- ban.sh
+++ ban.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #wget -O - http://cdn.rawgit.com/TTLMaster/yota-ban-list/master/list.txt | while read -r i; do
 wget -O - https://raw.githubusercontent.com/TTLMaster/yota-ban-list/master/list.txt | while read -r i; do
-	[ "`echo $i | sed -n '/^#.*$/p'`" ] && continue
+	[ "${i:0:1}" = "#" ] && continue
 	iptables -I FORWARD --destination $i -j REJECT;
 done
 /etc/init.d/dnsmasq restart
